624abe63182b2bfe6ad76feb65009ebac6325bbc
DanielYJ (YoungJae Lee) <leye195@naver.com>
2020-06-26 02:16:09 +0900

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,9 +3,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-token,hooks = os.getenv("SLACK_TOKEN"), os.getenv("WEBHOOKS")
+hooks = os.getenv("WEBHOOKS")
 
-def getToken():
-    return token
 def getWebHooks():
     return hooks
