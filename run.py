e4c483f8799ba4720d54c10c10a76d15a7962781
leye195 <leye195@naver.com>
2020-06-26 00:57:59 +0900

    #5. selenium , webdriver options 수정

--- run.py
+++ run.py
@@ -8,7 +8,12 @@ from settings import getToken
 slack = WebClient(getToken())
 
 options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+options.add_argument("lang=ko_KR")
 options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_argument("--no-sandbox")
 options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
 driver = webdriver.Chrome("chromedriver",options=options)
