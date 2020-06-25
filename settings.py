794f57b58b970938e6138d1388f5e99734f0b740
leye195 <leye195@naver.com>
2020-06-26 00:42:29 +0900

    #3. settings.py 코드 수정

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-token = os.environ.get("SLACK_TOKEN")
+token = os.getenv("SLACK_TOKEN")
 
 def getToken():
     return token
